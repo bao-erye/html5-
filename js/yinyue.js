5468f4fc86529427bc66be41e4dec48fdafdf551
FlashNight515 <2391748033@qq.com>
2020-03-19 15:51:46 +0800

    第一次提交

---
+++ js/yinyue.js
@@ -0,0 +1,96 @@
+// JavaScript Document<script language="javascript">   
+
+   function a(){
+     var audio = document.getElementById('musicA'); 
+     if(audio.paused){                 
+         audio.play();//a 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+   }
+   function b(){
+     var audio = document.getElementById('musicB'); 
+     if(audio.paused){                 
+         audio.play();//a 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+   }
+   function c(){
+     var audio = document.getElementById('musicC'); 
+     if(audio.paused){                 
+         audio.play();//a 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+   }
+   function d(){
+     var audio = document.getElementById('musicD'); 
+     if(audio.paused){                 
+         audio.play();//a 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+   }
+   function e(){
+     var audio = document.getElementById('musicE'); 
+     if(audio.paused){                 
+         audio.play();//a 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+   }
+   function f(){
+     var audio = document.getElementById('musicF'); 
+     if(audio.paused){                 
+         audio.play();//a 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+   }
+   function g(){
+     var audio = document.getElementById('musicG'); 
+     if(audio.paused){                 
+         audio.play();//a 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+   }
+   function h(){
+     var audio = document.getElementById('musicH'); 
+     if(audio.paused){                 
+         audio.play();//a 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+   }
+   function i(){
+     var audio = document.getElementById('musicI'); 
+     if(audio.paused){                 
+         audio.play();//a 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+   }
+   function j(){
+     var audio = document.getElementById('musicJ'); 
+     if(audio.paused){                 
+         audio.play();//a 播放  
+     }
+     else{
+          audio.pause();// 暂停
+     } 
+   }
+   function tijiao(){
+    
+    alert("提交成功！");
+  }
